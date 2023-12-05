b9ac4022af75c63ab20b3512bcabf679d3978e69
Dat Nguyen <dnguye08@g.uafs.edu>
2023-12-05 12:12:26 -0600

    updated - dat

---
+++ Role.php
@@ -0,0 +1,17 @@
+<?php
+class Role {
+    protected $permissions;
+
+    protected function construct() {
+        $this->permissions = array();
+
+    }
+
+    public static function getRolePerm($role_id){
+        $role = new Role();
+        
+    }
+
+
+}
+?>
