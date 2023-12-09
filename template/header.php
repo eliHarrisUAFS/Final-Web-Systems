77d6e70bb243cc9ad2997be94865af92b9103ebc
Dat Nguyen <dnguye08@g.uafs.edu>
2023-12-09 16:15:00 -0600

    updated

--- template/header.php
+++ template/header.php
@@ -1,28 +1,35 @@
 <?php
-   include "/home/student/projects/Final-Web-Systems/models/User.php";
+   //include "/home/student/projects/Final-Web-Systems/models/User.php";
+   //include "/home/student/projects/Final-Web-Systems/models/UserDAO.php";
 
-   $users = $_REQUEST['users'];
+    $users = $_REQUEST['users'];
     session_start();
+
     $user = new User();
-    $_SESSION['role'] = $user->getUrole();
     $user->load($row);
 
+    $userDAO = new UserDAO();
+    //$userData = $userDAO->authenticate($username, $password);
 
-    $role = $user->getUrole();
+    if ($userData){
+        $_SESSION['loggedin'] = true;
+        $_SESSION['role'] = $userData['urole'];
+        $_SESSION['userID'] = $userdata['userID'];
+        $_SESSION['user'] = $userData;
+    }
+    else {
+        $_SESSION['loggedin'] = false;
+    } 
 
+    if(isset($_SESSION['loggedin'])){
+        $status="Logged In";
+        $class="disabled";
+    }
 
-  if(isset($_SESSION['loggedin'])){
-    $status="Logged In";
-    $class="disabled";
-  }
-
-  else {
-    $status="Logged Out";
-    $class="enabled";
-  }
-
-    
-
+    else {
+        $status="Logged Out";
+        $class="enabled";
+    }
     
     $roleCheck = true;
 
@@ -47,11 +54,11 @@
                 <li class="nav-item">
                     <a class="nav-link" href="controller.php?page=home">Home</a>
                 </li>
-              <?php if (isset($_SESSION['loggedin']) && $_SESSION['role'] == 'admin') { ?>
+              
                 <li class="nav-item">
                     <a class="nav-link" href="controller.php?page=list">Admin</a>
                 </li>
-            <?php } ?>
+           
 
                 <li class="nav-item">
                     <a class="nav-link" href="controller.php?page=about">About</a>
